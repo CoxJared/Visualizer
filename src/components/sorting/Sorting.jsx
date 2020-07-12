c1099afa4b44e6909ba4f2ede091609e2596beaf
Jared Cox <coxj1@mcmaster.ca>
2020-07-12 09:20:46 -0400

    bubble sort basics

--- src/components/sorting/Sorting.jsx
+++ src/components/sorting/Sorting.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './Sorting.css'
 
-
+const BASE_COLOR = 'rgb(16, 145, 251)';
 
 export class Sorting extends Component {
   constructor(props) {
@@ -11,20 +11,45 @@ export class Sorting extends Component {
     }
   }
 
-
+  randomize() {
+    const MAX = 500;
+    let newShuffledBars = this.state.bars;
+    for(let i = 0; i < newShuffledBars.length; i++) {
+      newShuffledBars[i] = Math.floor(Math.random() * MAX);
+    }
+    this.setState({bars: newShuffledBars});
+  }
 
   bubbleSort() {
     let newBars = this.state.bars
     for (let i = 0; i < newBars.length - 1; i++) {
+
+
+      
+
+
       if(newBars[i] > newBars[i + 1]) {
         let tempBar = newBars[i];
         newBars[i] = newBars[i+1];
         newBars[i + 1] = tempBar;
+        this.highlightbar(newBars[i], 'rgb(255, 0, 0)')
       }
+
+
     }
     console.log(newBars)
     this.setState({bars: newBars})
-    // console.log(this.state.bars)
+  }
+
+  highlightbar(i, color) {
+    setTimeout(function() {
+      console.log('hi')
+      document.getElementById(`bar-${i}`).style.backgroundColor = color;
+    },0);
+    setTimeout(function() {
+      console.log('hi')
+      document.getElementById(`bar-${i}`).style.backgroundColor = 'rgb(0,255,0)';
+    },500);
   }
 
   render() {
@@ -38,6 +63,11 @@ export class Sorting extends Component {
           ))}
         </div>
         <div className="sorting-menu">
+        <div className="shuffle-button" onClick={this.randomize.bind(this)}> 
+            <h1>
+              Shuffle
+            </h1>
+          </div>
           <div className="bubble-sort-button" onClick={this.bubbleSort.bind(this)}> 
             <h1>
               Bubble Sort
